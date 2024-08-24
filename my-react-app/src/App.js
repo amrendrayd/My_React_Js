f09c747ca06d4f96e086a2b913a077c02d731ba7
amrendrayd <amrendrayd3@gmail.com>
2024-08-24 20:40:09 +0530

    Color changer screen project through reast using tailwind

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,30 +1,39 @@
 import './App.css';
-import Card from './cardComponent/Card';
-import Counter from './counter/Counter';
+// import Card from './cardComponent/Card';
+import Color from './colorChange/Color';
+// import Counter from './counter/Counter';
 
 function App() {
-    const gridBox = {
-        display: "grid",
-        gridTemplateColumns: "repeat(3, 1fr)",
-    };
+
+    // const gridBox = {
+    //     display: "grid",
+    //     gridTemplateColumns: "repeat(3, 1fr)",
+    // };
     return (
-        <div style={gridBox} className="App">
-            {/* <Counter/> */}
-            <Card
-                name="Amrendra"
-                email="amrendra@gmail.com"
-                image="https://via.placeholder.com/200"
-            />
-            <Card
-                name="Ashish"
-                email="ashish@gmail.com"
-                image="https://via.placeholder.com/200"
-            />
-            <Card
-                name="Sinu"
-                email="sinu@gmail.com"
-                image="https://via.placeholder.com/200"
-            />
+        // <div>
+        //     <Counter/>
+        // </div>
+
+        // <div style={gridBox} className="App">
+        //     <Card
+        //         name="Amrendra"
+        //         email="amrendra@gmail.com"
+        //         image="https://via.placeholder.com/200"
+        //     />
+        //     <Card
+        //         name="Ashish"
+        //         email="ashish@gmail.com"
+        //         image="https://via.placeholder.com/200"
+        //     />
+        //     <Card
+        //         name="Sinu"
+        //         email="sinu@gmail.com"
+        //         image="https://via.placeholder.com/200"
+        //     />
+        // </div>
+
+        <div>
+            <Color/>
         </div>
     );
 }
