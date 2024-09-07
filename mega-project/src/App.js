642bb1b676d56c68368830ef07d5d8313211620b
amrendrayd <amrendrayd3@gmail.com>
2024-09-07 15:18:04 +0530

    Configuratin setup of our project

---
+++ mega-project/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  console.log(process.env.REACT_APP_APPWRITE_URL);
+  
+  return (
+    <div className="App">
+      <h1>Amrendra</h1>
+    </div>
+  );
+}
+
+export default App;
